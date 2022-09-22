e824f6f592b5f1b4f5df426dc5c71cd4eb04b53d
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-09-22 15:40:59 +0000

    Read in data

--- .ccls-cache/@home@runner@HW5P2/main.cpp
+++ .ccls-cache/@home@runner@HW5P2/main.cpp
@@ -6,5 +6,12 @@ using namespace std;
 
 int main() {
  // load input file 
-  
+  ifstream inFile; // declare stream variable
+  inFile.open("NumberData1.txt");
+
+  // load all data in a while loop
+  while (!inFile.eof())
+    {
+      
+    }
 }
