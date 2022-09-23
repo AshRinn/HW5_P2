51b4bd31c0ca742156a316b865a7de0fcfc137e6
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-09-23 15:34:08 +0000

    Added if else into while loop

--- main.cpp
+++ main.cpp
@@ -10,11 +10,25 @@ int main() {
   inFile.open("NumberData1.txt");
 // declared variables 
   int dataTemp;
+  int sumData = 0;
+  int counter = 0;
   // load all data in a while loop
   while (inFile >> dataTemp)
     {
-      
-      cout << dataTemp << endl;
-      
+     // cout << dataTemp << endl;
+
+      //check to see if the data is valid 
+      if (dataTemp >= 0)
+      {
+        // update cumulative value of all data and number of data points loaded 
+        sumData = sumData + dataTemp;
+        counter++;
+      }
+      else // the value is negitive and we should exit
+      {
+        cout << "Negitive value encounted of " << dataTemp << "." << endl;
+        cout << "Will not use rest of the data..." << endl;
+        break;
+      }
     }
 }
