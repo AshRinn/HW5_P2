39af5b4068dd7b73ad67d8bd513bfbc6317d606b
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-09-23 15:40:52 +0000

    changed average to be a type double

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main() {
         break;
       }
     }
-  cout<< "The sume of the valid data is: "<< sumData<< endl;
+  cout<< "The sum of the valid data is: "<< sumData<< endl;
   cout << "The number of valid data points is: "<< counter<< endl;
-  cout << "The average of the valid data is: "<< (sumData/counter)<< endl;
+  cout << "The average of the valid data is: "<< (static_cast<double>(sumData)/counter)<< endl;
 }
